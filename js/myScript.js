361ccf5bf89998925fd6be9d2d427cb4e0a46bf8
Marius Pop <30757467+mariusCristianPop@users.noreply.github.com>
2022-02-21 20:30:38 +0100

    added html table
    Added html table and a few helper functions

--- js/myScript.js
+++ js/myScript.js
@@ -1,98 +1,138 @@
 document.addEventListener("DOMContentLoaded", function () {
     // wait for the html page to load before executing the script or else appending to body will result in an error;
-    var numberOfBombs = 35;
-    populateArrayWithBombs(numberOfBombs);
-    showMatrix(); 
+    deployBombs(numberOfBombs);
+    tableCreate();
 });
 
+const numberOfBombs = 35;
+const arrayDim = 12;
+
+
 // Create one dimensional array
-var gfg = new Array(10);
-  
-document.write("Creating 2D array <br>");
-  
+var gameBoardArray = new Array(arrayDim);
 // Loop to create 2D array using 1D array
-for (var i = 0; i < gfg.length; i++) {
-    gfg[i] = new Array(10);
+for (var i = 0; i < gameBoardArray.length; i++) {
+    gameBoardArray[i] = new Array(arrayDim);
 }
-  
-var h = 0;
   
 // Loop to initialize 2D array elements.
-for (var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++) {
-        gfg[i][j] = h;
+for (var i = 0; i < arrayDim; i++) {
+    for (var j = 0; j < arrayDim; j++) {
+        gameBoardArray[i][j] = 0;
     }
-}
-  
-// Loop to display the elements of 2D array. 
-for (var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++)    {
-        document.write(gfg[i][j] + " ");
-    }
-    document.write("<br>");
 }
 
-function populateArrayWithBombs(numberOfBombs) {
-    for (var i = 0; i < 10 && numberOfBombs > 0; i++) {
-        for (var j = 0; j < 10 && numberOfBombs > 0; j++) {
-            if (gfg[i][j] != 1 && placeBomb()) {
-                gfg[i][j] = 1;
+// Deploy bombs in table
+function deployBombs(numberOfBombs) {
+    for (var i = 1; i < arrayDim - 1 && numberOfBombs > 0; i++) {
+        for (var j = 1; j < arrayDim - 1 && numberOfBombs > 0; j++) {
+            if (gameBoardArray[i][j] != 1 && placeBomb()) {
+                gameBoardArray[i][j] = 1;
                 --numberOfBombs;
             }            
         }
     }
     if (numberOfBombs) { // if not all bombs deployed
-        populateArrayWithBombs(numberOfBombs);
+        deployBombs(numberOfBombs);
     }
 }
 
+// Decide if a table cell shall hold a bomb or not based on a given probability
 function placeBomb() {
-    let notRandomNumbers = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]; // Here I decide the probability of 0 or 1. 
+    let notRandomNumbers = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]; // Here I decide the probability of 0's or 1's. 
     let id = Math.floor(Math.random() * notRandomNumbers.length);
     return notRandomNumbers[id];
 }
 
-function showMatrix() {
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++)    {
-            document.write(gfg[i][j] + " ");
-        }
-        document.write("<br>");
-    }
-}
-
+// Counts the bombs all around a given cell
 function countNeighbourBombs(i, j) {
     let counter = 0;
-    if (gfg[i][j + 1]) {
-        counter++;
-    }
-    if (gfg[i - 1][j + 1]) {
-        counter++;
-    }
-    if (gfg[i - 1][j - 1]) {
-        counter++;
-    }
-    if (gfg[i + 1][j - 1]) {
-        counter++;
-    }
-    if (gfg[i + 1][j + 1]) {
-        counter++;
-    }
-    if (gfg[i - 1][j]) {
-        counter++;
-    }
-    if (gfg[i][j - 1]) {
-        counter++;
-    }
-    if (gfg[i + 1][j]) {
-        counter++;
+    if (i > 0 && j > 0) {
+        if (gameBoardArray[i][j + 1]) {
+            counter++;
+        }
+        if (gameBoardArray[i - 1][j + 1]) {
+            counter++;
+        }
+        if (gameBoardArray[i - 1][j - 1]) {
+            counter++;
+        }
+        if (gameBoardArray[i + 1][j - 1]) {
+            counter++;
+        }
+        if (gameBoardArray[i + 1][j + 1]) {
+            counter++;
+        }
+        if (gameBoardArray[i - 1][j]) {
+            counter++;
+        }
+        if (gameBoardArray[i][j - 1]) {
+            counter++;
+        }
+        if (gameBoardArray[i + 1][j]) {
+            counter++;
+        }
     }
     return counter;
 }
 
-function hasBombsAround(i, j) {
-    return (gfg[i][j + 1]) || (gfg[i - 1][j + 1]) || (gfg[i - 1][j]) || (gfg[i - 1][j - 1]) ||
-            (gfg[i][j - 1]) || (gfg[i + 1][j - 1]) || (gfg[i + 1][j]) || (gfg[i + 1][j + 1]);
+// create the HTML table
+function tableCreate() {
+    const body = document.body,
+        table = document.createElement('table');
 
+    for (let i = 1; i <= arrayDim - 2; i++) {
+        const tr = table.insertRow();
+        for (let j = 1; j <= arrayDim - 2; j++) {
+            const td = tr.insertCell();
+            td.setAttribute("id", i * 10 + j);
+            td.addEventListener("contextmenu", (event) => {
+                event.preventDefault();
+                addFlag(i, j);
+            }, { once: true }); // added so that this event listener only executes once and ads a flag
+            td.setAttribute("onclick", `leftClick(${i}, ${j})`);
+
+        }
+    }
+    body.appendChild(table);
+}
+
+// add a flag on HTML table on right click
+function addFlag(i, j) {
+    gameBoardArray[i][j] = 0;
+    var tableCellId = document.getElementById(`${i * 10 + j}`);
+    var image = document.createElement("img");
+    image.src = "assets/icons/flag.png";
+    tableCellId.appendChild(image);
+}
+
+// add a bomb on HTML table
+function addBomb(i, j) {
+    console.log(`left click on ${i} ${j}`);
+    var tableCellId = document.getElementById(`${i * 10 + j}`);
+    tableCellId.style.background = "red";
+    var image = document.createElement("img");
+    image.src = "assets/icons/bomb.png";
+    tableCellId.appendChild(image);
+}
+
+// add the number of adiacent cells that hav a bomb on HTML table
+function addNumber(i, j) {
+    let number = countNeighbourBombs(i, j);
+    var tableCellId = document.getElementById(`${i * 10 + j}`);
+    tableCellId.style.background = "white";
+    tableCellId.innerText = number;
+}
+
+// test game conditions when left click is detected
+function leftClick(i, j) {
+    console.log("left click detected " + i + " " + j);
+    var tableCellId = document.getElementById(`${i * 10 + j}`);
+    tableCellId.setAttribute("onclick", "");
+    if (gameBoardArray[i][j] == 1) {
+        addBomb(i, j);
+    } else {
+        addNumber(i, j);
+    } 
 }
   
